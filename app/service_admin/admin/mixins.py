f22ce40c80c978e6a5ff1dd99f3ba8191b3a6337
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-04-07 00:03:29 +0100

    adds permission mixin

---
+++ app/service_admin/admin/mixins.py
@@ -0,0 +1,48 @@
+#!/usr/bin python3
+
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Python:
+
+# 3rd party:
+from django.conf import settings
+
+# Internal: 
+from service_admin.utils.presets import Environment
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+__all__ = [
+    'ProdOnlyAdd',
+    'ProdOnlyDelete',
+    'ProdOnlyChange',
+    'ProdOnlyOps'
+]
+
+
+class ProdOnlyAdd:
+    def has_add_permission(self, request):
+        if settings.ENVIRONMENT != Environment.PRODUCTION:
+            return False
+
+        return super().has_add_permission(request)
+
+
+class ProdOnlyDelete:
+    def has_delete_permission(self, request, obj=None):
+        if settings.ENVIRONMENT != Environment.PRODUCTION:
+            return False
+
+        return super().has_delete_permission(request, obj)
+
+
+class ProdOnlyChange:
+    def has_change_permission(self, request, obj=None):
+        if settings.ENVIRONMENT != Environment.PRODUCTION:
+            return False
+
+        return super().has_change_permission(request, obj)
+
+
+class ProdOnlyOps(ProdOnlyAdd, ProdOnlyChange, ProdOnlyDelete):
+    pass
