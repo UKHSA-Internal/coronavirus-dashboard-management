d2888849d589aa48b8a8d8298b89db4a5996389f
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2022-01-30 15:43:52 +0000

    adds custom login URL

--- app/service_admin/urls.py
+++ app/service_admin/urls.py
@@ -1,6 +1,6 @@
 #!/usr/bin python3
 
-from django.urls.conf import path, include, re_path
+from django.urls.conf import path
 from django.contrib.admin.views.decorators import staff_member_required
 
 from service_admin import views
@@ -8,6 +8,7 @@ from service_admin import views
 
 # ToDo: Add API Documentations.
 urlpatterns = [
+    path('login', views.ProtectedLoginView.as_view()),
     path('textstats', views.text_stats_api_view, name="textstats"),
     path('etl_status', staff_member_required(views.EtlView.as_view(), login_url="/login"), name='etlview')
 ]
