c7500e122fa40227e1c920f3f780ee48b5f65972
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2022-01-30 18:43:29 +0000

    inherits admin auth form

--- app/service_admin/views/login.py
+++ app/service_admin/views/login.py
@@ -5,8 +5,9 @@
 # Python:
 
 # 3rd party:
-from django.contrib.auth.forms import AuthenticationForm
+
 from django.contrib.auth.views import LoginView
+from django.contrib.admin.forms import AdminAuthenticationForm
 
 from snowpenguin.django.recaptcha2.fields import ReCaptchaField
 from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
@@ -20,14 +21,15 @@ __all__ = [
 ]
 
 
-class ProtectedAuthenticationForm(AuthenticationForm):
+class ProtectedAuthenticationForm(AdminAuthenticationForm):
     captcha = ReCaptchaField(
         widget=ReCaptchaWidget(),
+        label='recaptcha',
         error_messages={'required': "Please use our fancy tool to prove you're not a robot!"}
     )
 
 
 class ProtectedLoginView(LoginView):
+    template_name = 'admin/login.html'
     form_class = ProtectedAuthenticationForm
-
 
